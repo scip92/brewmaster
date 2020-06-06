8bdf042e37ead339254455781176804f69428fb0
scip92 <scipm5592@gmail.com>
2020-06-07 00:10:46 +0200

    Add initial main.ts

---
+++ main.ts
@@ -0,0 +1,15 @@
+import express from "express";
+const app = express();
+const port = 8080; // default port to listen
+
+// define a route handler for the default home page
+app.get( "/", ( req, res ) => {
+    // render the index template
+    res.render( "index" );
+} );
+
+// start the express server
+app.listen( port, () => {
+    // tslint:disable-next-line:no-console
+    console.log( `server started at http://localhost:${ port }` );
+} );
