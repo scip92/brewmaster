dd88016f3d013ac7b34ecaefc707dbec4c33b727
scip92 <scipm5592@gmail.com>
2020-06-07 14:15:27 +0200

    Fix cors middleware

--- server/src/no-cors.ts
+++ server/src/no-cors.ts
@@ -1,6 +1,6 @@
-import { Request, Response } from "express";
+import { Request, Response, NextFunction } from "express";
 
-export const noCors = (res: Request, _: Response, next) => {
+export const noCors = (_: Request, res: Response, next: NextFunction) => {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
