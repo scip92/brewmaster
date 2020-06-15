391dbd44d48c899b9a40b5619a84f8f2d526b9a0
BHRnR <benni.hacker@gmail.com>
2020-06-16 00:07:27 +0200

    Display history list on front end

--- server/src/app.ts
+++ server/src/app.ts
@@ -6,25 +6,23 @@ var fs = require('fs');
 const app = express();
 const filePath = "C:/Users/Benjamin Hacker/Desktop/Programmieren/temperature"
 const getRandomTemperature = true
-const refreshTime = 0.5
-const temperatures = []
+const refreshTime = 1
+const temperatures: number[] = []
 
-function readCurrentTemperature():number {
-    let temperature
+function readCurrentTemperature(): number {
     if (getRandomTemperature) {
-        temperature = Math.random() * 80 + 20;
+        return Math.random() * 80 + 20;
     } else {
         const readString = fs.readFileSync(filePath).toString()
-        temperature = readString.split("t=")[1] / 1000
+        return readString.split("t=")[1] / 1000
     }
-    return temperature
 }
 
 setInterval(() => {
     console.log("eine Sekunde")
     temperatures.push(readCurrentTemperature())
     console.log(temperatures)
-},1000*refreshTime)
+}, 1000 * refreshTime)
 
 app.set("port", 5000);
 
@@ -35,7 +33,11 @@ app.get("/", (req: Request, res: Response) => {
 });
 
 app.get("/temperature", (req: Request, res: Response) => {
-    res.send(temperatures[temperatures.length-1].toFixed(2).toString())
+    res.send(temperatures[temperatures.length - 1].toFixed(2).toString())
+});
+
+app.get("/temperatures", (req: Request, res: Response) => {
+    res.send(temperatures.map(t => t.toFixed(2)));
 });
 
 export default app;
