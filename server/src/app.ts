be0fca17354130a738d59f5207617df3e6337230
BHRnR <benni.hacker@gmail.com>
2020-06-15 23:02:25 +0200

    #5: Save temperatures in array

--- server/src/app.ts
+++ server/src/app.ts
@@ -5,7 +5,26 @@ import { noCors } from "./no-cors";
 var fs = require('fs');
 const app = express();
 const filePath = "C:/Users/Benjamin Hacker/Desktop/Programmieren/temperature"
-var getRandomTemperature = false
+const getRandomTemperature = true
+const refreshTime = 0.5
+const temperatures = []
+
+function readCurrentTemperature():number {
+    let temperature
+    if (getRandomTemperature) {
+        temperature = Math.random() * 80 + 20;
+    } else {
+        const readString = fs.readFileSync(filePath).toString()
+        temperature = readString.split("t=")[1] / 1000
+    }
+    return temperature
+}
+
+setInterval(() => {
+    console.log("eine Sekunde")
+    temperatures.push(readCurrentTemperature())
+    console.log(temperatures)
+},1000*refreshTime)
 
 app.set("port", 5000);
 
@@ -16,14 +35,7 @@ app.get("/", (req: Request, res: Response) => {
 });
 
 app.get("/temperature", (req: Request, res: Response) => {
-    if (getRandomTemperature) {
-        const temperature = Math.random() * 80 + 20;
-        res.send(temperature.toFixed(2).toString());
-    } else {
-        const readString = fs.readFileSync(filePath).toString()
-        const temperature = readString.split("t=")[1]/1000
-        res.send(temperature.toFixed(2).toString())
-    }
+    res.send(temperatures[temperatures.length-1].toFixed(2).toString())
 });
 
 export default app;
