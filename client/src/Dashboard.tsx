9495b01544e74e2776f3a5a7bc1dc97510634c57
scip92 <scipm5592@gmail.com>
2020-06-16 18:21:35 +0200

    #5 cleanup obsolete code

--- client/src/Dashboard.tsx
+++ client/src/Dashboard.tsx
@@ -1,13 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { Typography, Box } from "@material-ui/core";
-import { LineChart, Line, CartesianGrid, XAxis, YAxis } from "recharts";
+import { LineChart, Line, XAxis, YAxis } from "recharts";
 import { Measurement } from "../../shared/measurement";
-
-const data = [
-  { name: "Page A", uv: 400, pv: 2400, amt: 2400 },
-  { name: "Page B", uv: 200, pv: 2400, amt: 2400 },
-  { name: "Page C", uv: 300, pv: 2400, amt: 2400 },
-];
 
 export function Dashboard() {
   const [temperature, setTemperature] = useState(0);
@@ -38,7 +32,7 @@ export function Dashboard() {
       </Typography>
       <LineChart width={400} height={400} data={temperatures}>
         <Line type="monotone" dataKey="value" stroke="#8884d8" />
-        <XAxis dataKey="name" />
+        <XAxis dataKey="timestamp" />
         <YAxis />
       </LineChart>
     </Box>
