f8ad8f7b7afe3b91e51b5c6620332c79533ccd40
scip92 <scipm5592@gmail.com>
2020-06-24 22:37:53 +0200

    Cleanup server, use correct file for hosting

--- server/src/app.ts
+++ server/src/app.ts
@@ -13,9 +13,9 @@ const app = express();
 const filePath = process.env.SENSOR_PATH;
 const useFake = (process.env.USE_FAKER === "true");
 const measurementInterval = parseFloat(process.env.MEASUREMENT_INTERVAL)
-const dataStore = new Datastore({autoload: true, filename: "./data/temperatures.db"});
+const dataStore = new Datastore({ autoload: true, filename: "./data/temperatures.db" });
 dataStore.loadDatabase();
- 
+
 function readCurrentTemperature(): number {
     if (useFake) {
         return Math.random() * 80 + 20;
@@ -26,23 +26,22 @@ function readCurrentTemperature(): number {
 }
 
 setInterval(() => {
-    dataStore.insert({value: readCurrentTemperature(), timestamp: new Date()});
+    dataStore.insert({ value: readCurrentTemperature(), timestamp: new Date() });
 }, 1000 * measurementInterval)
 
 app.set("port", process.env.PORT);
 
 app.use(noCors);
-
-app.get("/", (req: Request, res: Response) => {
-    res.send("Hello World!");
-});
+app.use(express.static("public"));
 
 app.get("/temperature", async (req: Request, res: Response) => {
-    res.send(readCurrentTemperature());
+    dataStore.find({}).sort({ timestamp: -1 }).limit(1).exec((err: Error, doc: any) => {
+        res.send(doc[0]);
+    })
 });
 
 app.get("/temperatures", async (req: Request, res: Response) => {
-    dataStore.find({}, (err: Error, all: any) =>{
+    dataStore.find({}, (err: Error, all: any) => {
         res.send(JSON.stringify(all));
     })
 });
