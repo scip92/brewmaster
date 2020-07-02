1055a003caba3beab76b44b075c799e3a49d26c8
BHRnR <benni.hacker@gmail.com>
2020-07-02 16:38:50 +0200

    Add port number to config

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from server import create_app
+from server import port_number
 
 if __name__ == '__main__':
-    create_app().run(debug=True, host='0.0.0.0', port=5000)
+    create_app().run(debug=True, host='0.0.0.0', port=port_number)
--- server/config_exmaple.py
+++ server/config_exmaple.py
@@ -1,2 +1,3 @@
 use_fake = True
 temp_sensor_path = "/sys/bus/w1/devices/{your_sensor_id}/w1_slave"
+port_number = 5000
