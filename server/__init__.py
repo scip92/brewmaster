1b95efad9abeb27b1af60c78ca144f59e1c1f1fb
BHRnR <benni.hacker@gmail.com>
2020-07-03 13:47:14 +0200

    Implement tinydb initially

--- server/__init__.py
+++ server/__init__.py
@@ -1,4 +1,5 @@
 from .config import temp_sensor_path
 from .config import use_fake
 from .config import port_number
+from .config import database_path
 from .app import create_app
--- server/app.py
+++ server/app.py
@@ -5,11 +5,14 @@ import datetime
 import time
 from flask import Flask, render_template, jsonify, request
 from flask_cors import CORS
+from tinydb import TinyDB, Query
 
 from .display import get_display
 from .sensor import get_temperature_sensor
 from .sensor.fake_temperature_sensor import FakeTemperatureSensor
 from .sensor.temperature_sensor import TemperatureSensor
+
+from . import database_path
 
 
 POOL_TIME = 2
@@ -17,17 +20,17 @@ POOL_TIME = 2
 current_temperature = 0
 dataLock = threading.Lock()
 thread = threading.Thread()
+db = TinyDB(database_path)
 
 
 def create_app():
     app: Flask = Flask(__name__)
     CORS(app)
 
-
     @app.route("/temperature")
     def temp():
         global current_temperature
-        return jsonify(value = current_temperature, timestamp = str(datetime.datetime.now()) )
+        return jsonify(value=current_temperature, timestamp=str(datetime.datetime.now()))
 
     def stop_thread():
         global thread
@@ -39,8 +42,10 @@ def create_app():
         display = get_display()
         while True:
             current_temperature = temp_sensor.read_temperature()
+            current_time = str(datetime.datetime.now())
             display.display_text(str(current_temperature))
             time.sleep(POOL_TIME)
+            db.insert({'value': current_temperature, 'timestamp': current_time})
 
     def start_threat():
         global thread
