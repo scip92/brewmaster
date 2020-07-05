516f3c68528c19d64fb703da89be23688c75ff48
scip92 <scipm5592@gmail.com>
2020-07-05 16:44:37 +0200

    Implement initial post request to save target value

--- server/app.py
+++ server/app.py
@@ -1,6 +1,5 @@
 import threading
-import datetime
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 from flask_cors import CORS
 from tinydb import TinyDB
 
@@ -9,6 +8,8 @@ from server import database_path
 dataLock = threading.Lock()
 thread = threading.Thread()
 db = TinyDB(database_path)
+
+targetValue = 57
 
 
 def create_app() -> Flask:
@@ -19,5 +20,15 @@ def create_app() -> Flask:
     def temp():
         all_temperatures = db.all()
         return jsonify(all_temperatures[-1])
+
+    @app.route("/target-temperature", methods=["GET", "POST"])
+    def target_value():
+        global targetValue
+        if request.method == "POST":
+            new_value = request.get_json()["new_value"]
+            print("new value set", new_value)
+            if new_value:
+                targetValue = new_value
+        return str(targetValue)
 
     return app
