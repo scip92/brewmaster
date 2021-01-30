13076406596eecad00c6bd800693878b9f2622dc
scip92 <scipm5592@gmail.com>
2021-01-30 17:21:33 +0100

    Implement error handling for temperature sensor

--- server/sensor/temperature_sensor.py
+++ server/sensor/temperature_sensor.py
@@ -1,3 +1,5 @@
+import logging
+import sys
 from injector import inject
 
 from ..config import temp_sensor_path
@@ -10,10 +12,14 @@ class TemperatureSensor:
         self.path = temp_sensor_path
 
     def read_temperature(self) -> float:
-        file = open(self.path, 'r')
-        lines = file.readlines()
-        file.close()
-        temperature_str = lines[1].find('t=')
-        temperature_value = lines[1][temperature_str + 2:]
-        in_celsius = float(temperature_value) / 1000.0
-        return round(in_celsius, 1)
+        try:
+            file = open(self.path, 'r')
+            lines = file.readlines()
+            file.close()
+            temperature_str = lines[1].find('t=')
+            temperature_value = lines[1][temperature_str + 2:]
+            in_celsius = float(temperature_value) / 1000.0
+            return round(in_celsius, 1)
+        finally:
+            logging.info("Unexpected error:", sys.exc_info()[0])
+            return -1
